3466feb0787de160312ff3baecaa894e509a7244
ThetNaingTun <67203393+thetnaingtunerp@users.noreply.github.com>
2025-11-16 11:38:03 +0630

    impr

---
+++ WebcamTest/gendermodel.py
@@ -0,0 +1,17 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array
+from tensorflow.keras.preprocessing import image
+
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, BatchNormalization
+from tensorflow.keras.optimizers import Adam, RMSprop
+
+from sklearn.model_selection import train_test_split
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import os
+
